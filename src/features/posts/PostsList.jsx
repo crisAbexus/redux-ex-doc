af587f284b9d967f0ba8ee8e1ad1dcfe18937c5c
cris <ctovar@abexuspr.com>
2022-10-09 05:24:11 -0500

    🍕 Part5> Displaying Loading State

--- src/features/posts/PostsList.jsx
+++ src/features/posts/PostsList.jsx
@@ -1,9 +1,24 @@
 import React, { useEffect } from "react";
 import { PostAuthor } from "./PostAuthor";
+import { TimeAgo } from "./TimeAgo";
 import { useSelector, useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
 import { ReactionButtons } from "./ReactionButtons";
 import { selectAllPosts, fetchPosts } from "./postsSlice";
+import { Spinner } from '../../components/Spinner'
+
+const PostExcerpt = ({ post }) => {
+  return (
+    <article className="post-excerpt" key={post.id} >
+      <h3>{post.title}</h3>
+      <p className="post-content">{post.content.substring(0, 100)}</p>
+      <ReactionButtons post={post} />
+      <Link to={`/posts/${post.id}`} className="button muted-button">
+        View Post
+      </Link>
+    </article>
+  )
+}
 
 export const PostsList = () => {
   const posts = useSelector(selectAllPosts);
@@ -12,6 +27,9 @@ export const PostsList = () => {
   const postStatus = useSelector((state) => {
     return state.posts.status
   })
+  const error = useSelector((state) => {
+    return state.posts.error;
+  })
 
   useEffect(() => {
     if (postStatus === 'idle') {
@@ -19,23 +37,28 @@ export const PostsList = () => {
     }
   }, [postStatus, dispatch])
 
-  const renderedPosts = orderedPosts.map(post => {
-    return (
-      <article className="post-excerpt" key={post.id} >
-        <h3>{post.title}</h3>
-        <p className="post-content">{post.content.substring(0, 100)}</p>
-        <ReactionButtons post={post} />
-        <Link to={`/posts/${post.id}`} className="button muted-button">
-          View Post
-        </Link>
-        <PostAuthor userId={post.user} />
-      </article>
-    )
-  })
+  let content;
+
+  if (postStatus === 'loading') {
+    content = <Spinner text='Loading...' />
+  } else if (postStatus === 'succeeded') {
+    // sort posts in reverse chronological order by datetme string
+    const orderedPosts = posts
+      .slice()
+      .sort((a, b) => b.date.localeCompare(a.date));
+    content = orderedPosts.map((post) => {
+      return (
+        <PostExcerpt key={post.id} post={post} />
+      )
+    })
+  } else if (postStatus === 'failed') {
+    content = <div>{error}</div>
+  }
+
   return (
     <section className="posts-list">
       <h2>Post</h2>
-      {renderedPosts}
+      {content}
     </section>
   )
 }
