0c235c07a8a3670b2e62e5b4a2237f25e6e44d37
cris <ctovar@abexuspr.com>
2023-02-25 07:58:10 -0500

    🎂 part7>Query Arguments and Cache keys

--- src/features/posts/SinglePostPage.jsx
+++ src/features/posts/SinglePostPage.jsx
@@ -5,29 +5,35 @@ import { PostAuthor } from "./PostAuthor";
 import { ReactionButtons } from "./ReactionButtons";
 import { selectPostById } from "./postsSlice";
 
+import { Spinner } from '../../components/Spinner';
+import { useGetPostQuery } from '../api/apiSlice';
+
 export const SinglePostPage = ({ match }) => {
   const { postId } = match.params;
 
-  const post = useSelector(state => {
-    return selectPostById(state, postId);
-  })
+  const { data: post, isFetching, isSuccess } = useGetPostQuery(postId);
 
+  let content;
+  if (isFetching) {
+    content = <Spinner text="Loading..." />
+  } else if (isSuccess) {
+    return (
+      <section>
+        <article>
+          {(post) ? (
+            <>
+              <h2>{post.title}</h2>
+              <p className="post-content">{post.content}</p>
+              <ReactionButtons post={post} />
+              <Link to={`/editPost/${post.id}`} className="button">
+                Edit Post
+              </Link>
+              <PostAuthor userId={post.user} />
+            </>
+          ) : <h2>Post not found!</h2>}
+        </article>
+      </section>
+    );
+  }
 
-  return (
-    <section>
-      <article>
-        {(post) ? (
-          <>
-            <h2>{post.title}</h2>
-            <p className="post-content">{post.content}</p>
-            <ReactionButtons post={post} />
-            <Link to={`/editPost/${post.id}`} className="button">
-              Edit Post
-            </Link>
-            <PostAuthor userId={post.user} />
-          </>
-        ) : <h2>Post not found!</h2>}
-      </article>
-    </section>
-  )
 }
