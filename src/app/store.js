349f12c32b36e249123d61a65afbf9acfa62570f
cris <ctovar@abexuspr.com>
2023-02-25 06:48:20 -0500

    ❄️  part7>Define an API Slice

--- src/app/store.js
+++ src/app/store.js
@@ -2,11 +2,16 @@ import { configureStore } from '@reduxjs/toolkit'
 import postsReducer from "../features/posts/postsSlice";
 import usersSlice from "../features/users/usersSlice";
 import notificationsSlice from "../features/notifications/notificationsSlice";
+import { apiSlice } from "../features/api/apiSlice";
 
 export default configureStore({
   reducer: {
     posts: postsReducer,
     users: usersSlice,
     notifications: notificationsSlice,
+    [apiSlice.reducerPath]: apiSlice.reducer,
+  },
+  middleware: (getDefaultMiddleware) => {
+    return getDefaultMiddleware().concat(apiSlice.middleware);
   },
 })
