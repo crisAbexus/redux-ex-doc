dd4175d66ee2f4973f43288681d8a107221af935
cris <ctovar@abexuspr.com>
2023-02-25 15:47:31 -0500

    🪝 part7>Automatic refresh with cache invalidation

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -3,11 +3,13 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({ baseUrl: '/fakeApi' }),
+  tagTypes: ['Post'],
   endpoints: builder => ({
     getPosts: builder.query({
       query: () => {
         return '/posts'
       },
+      providesTags: ['Post'],
     }),
     getPost: builder.query({
       query: (postId) => {
@@ -21,7 +23,8 @@ export const apiSlice = createApi({
           method: 'POST',
           body: initialPost,
         }
-      }
+      },
+      invalidatesTags: ['Post'],
     })
   }),
 })
