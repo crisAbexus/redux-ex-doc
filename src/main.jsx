1ceb5ef0557d0d6885470039972eeba926bf4ba3
cris <ctovar@abexuspr.com>
2023-02-26 12:56:14 -0500

    😸 display endpoints on console and update the store

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App'
 import store from './app/store'
 import { Provider } from 'react-redux'
-import { fetchUsers } from "./features/users/usersSlice";
+import { apiSlice } from "./features/api/apiSlice";
 import { fetchNotifications } from "./features/notifications/notificationsSlice";
 
 import { worker } from './api/server'
@@ -14,8 +14,7 @@ async function main() {
   // Start our mock API server
   await worker.start({ onUnhandledRequest: 'bypass' })
 
-  store.dispatch(fetchUsers())
-  store.dispatch(fetchNotifications())
+  store.dispatch(apiSlice.endpoints.getUsers.initiate())
 
   const root = ReactDOM.createRoot(document.getElementById('root'));
   root.render(
