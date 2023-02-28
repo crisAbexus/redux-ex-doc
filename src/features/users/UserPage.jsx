4442322b9eb2af84b51df57aca6f7d12b80d1ec9
cris <ctovar@abexuspr.com>
2023-02-28 07:10:18 -0500

    🍻 select values from Results

--- src/features/users/UserPage.jsx
+++ src/features/users/UserPage.jsx
@@ -1,20 +1,43 @@
 import React from "react";
+import { createSelector } from "@reduxjs/toolkit";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
 import { selectUserById } from "../users/usersSlice";
+import { useGetPostsQuery } from "../api/apiSlice"
 import { selectAllPosts, selectPostByUser } from "../posts/postsSlice";
 
 export const UserPage = ({ match }) => {
   const { userId } = match.params;
 
   const user = useSelector(state => selectUserById(state, userId))
-  console.log(`🙏%cUserPage.jsx:12 - user`, 'font-weight:bold; background:#3cc300;color:#fff;'); //DELETEME
-  console.log(user); // DELETEME
 
-  const postsForUser = useSelector(state => {
-    const posts_by_user = selectPostByUser(state, userId)
-    return posts_by_user;
+  const selectPostsForUser = (useMemo) => {
+    return useMemo(() => {
+      const emptyArray = [];
+      return createSelector(
+        (res) => {
+          return res.data;
+        },
+        (res, userId) => {
+          return userId;
+        },
+        (data, userId) => {
+          const data_filtered_by_id = data?.filter((post) => {
+            return post.user === userId;
+          })
+          return data_filtered_by_id ?? emptyArray;
+        }
+      )
+    }, [])
+  }
+  const postsForUser = useGetPostsQuery(undefined, {
+    selectFromResult: (result) => {
+      return ({
+        ...result,
+        postsForUser: selectPostsForUser(result, userId),
+      })
+    }
   })
 
   const postTitles = postsForUser.map((post) => {
