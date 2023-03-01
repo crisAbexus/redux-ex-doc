349677bfe97b0f59f3ee12b0cb9bf8ba6922a5a3
cris <ctovar@abexuspr.com>
2023-03-01 06:55:38 -0500

    🪧 Stream Cache updates:
    the typical pattern for this implementation is:
    - ```await cacheDataLoaded``` right away.
    - Create a server-side data subscription like a WebSocket.
    - When an update is received, use updateCachedData to "mutate" the
    cached values based on the update.
    - ```await cacheEntryRemoved``` at the end.
    - Clean up subscriptions afterwards.

--- src/features/notifications/notificationsSlice.js
+++ src/features/notifications/notificationsSlice.js
@@ -1,5 +1,7 @@
 import { createSlice, createAsyncThunk, createEntityAdapter, } from "@reduxjs/toolkit";
 import { client } from "../../api/client";
+import { apiSlice } from "../api/apiSlice";
+import { forceGenerateNotifications } from "../../api/server";
 
 const notificationsAdapter = createEntityAdapter({
   sortComparer: (a, b) => b.date.localeCompare(a?.date),
@@ -41,6 +43,68 @@ const notificationsSlice = createSlice({
     });
   },
 });
+
+export const extendedApi = apiSlice.injectEndpoints({
+  endpoints: (builder) => {
+    return ({
+      getNotifications: builder.query({
+        query: () => '/notifications',
+        async onCacheEntryAdded(arg, { updateCachedData, cacheDataLoaded, cacheEntryRemoved }) {
+          // create a WebSocket connection when the cache subscription starts
+          const ws = new WebSocket('ws://localhost');
+          try {
+            // wait for the initial query to resolve before proceeding
+            await cacheDataLoaded
+            // when data is received from the socket connection to the server, update our query result with the received message
+            const listener = (event) => {
+              const message = JSON.parse(event.data);
+              switch (message.type) {
+                case 'notifications': {
+                  updateCachedData(draft => {
+                    // Insert all received notifications from the websocket into the existing RTKQ cache array
+                    draft.push(...message.payload);
+                    draft.sort((a, b) => {
+                      return b.date.localeCompare(a.date);
+                    })
+                  })
+                  break
+                }
+                default:
+                  break
+              }
+              ws.addEventListener('message', listener);
+            }
+          } catch {
+
+          }
+          await cacheEntryRemoved;
+          ws.close();
+        }
+      })
+    })
+  }
+})
+
+export const { useGetNotificationsQuery } = extendedApi;
+const emptyNotifications = [];
+export const selectNotificationsResult = extendedApi.endpoints.getNotifications.select();
+
+const selectNotificationsData = createSelector(
+  selectNotificationsResult,
+  (notificationsResult) => {
+    return (notificationsResult.data ?? emptyNotifications);
+  }
+)
+
+export const fetchNotificationsWebsocket = () => {
+  return (dispatch, getState) => {
+    const allNotifications = selectNotificationsData(getState());
+    const [latestNotification] = allNotifications
+    const latestTimestamp = latesNotification?.date ?? "";
+    // Hardcode a call to the mock server to simulate a server push scenario over websockets
+    forceGenerateNotifications(latestTimestamp);
+  }
+}
 
 export const { allNotificationsRead } = notificationsSlice.actions;
 
